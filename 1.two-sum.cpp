593bed6c90aa3806272ddba2622ffeab4cf45934
hmmei <ming197@qq.com>
2024-06-04 02:25:41 +0800

    hash

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -5,10 +5,24 @@
  */
 
 // @lc code=start
+#include "bits/stdc++.h"
+using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+        // use a map to store the index of each number
+        unordered_map<int, int> num2idx;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            int num = nums[i];
+            int complement = target - num;
+            if (num2idx.find(complement) != num2idx.end())
+            {
+                return {num2idx[complement], i};
+            }
+            num2idx[num] = i;
+        }
+        return {};
     }
 };
 // @lc code=end
