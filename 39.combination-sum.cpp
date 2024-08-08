00ed93c68358bd25b377780f8ec94fdc0f9779be
hmmei <ming197@qq.com>
2024-08-08 18:11:54 +0800

    permutation and cannnot be duplicate

---
+++ 39.combination-sum.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=39 lang=cpp
+ *
+ * [39] Combination Sum
+ */
+
+// @lc code=start
+#include "bits/stdc++.h"
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> result;
+    vector<int> tmp;
+    int sum = 0;
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        DFS(candidates, target);
+        return result;
+    }
+
+    void DFS(vector<int>& candidates, int target, int start = 0) {
+        if (sum == target) {
+            result.push_back(tmp);
+            return;
+        }
+        // cannot be duplicated, so start from i
+        for (int i = start; i < candidates.size(); i++) {
+            if (sum + candidates[i] > target) {
+                return;
+            }
+            sum += candidates[i];
+            tmp.push_back(candidates[i]);
+            DFS(candidates, target, i);
+            sum -= candidates[i];
+            tmp.pop_back();
+        }
+    }
+};
+// @lc code=end
+
