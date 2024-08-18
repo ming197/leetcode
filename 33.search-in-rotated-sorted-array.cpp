4ffef7f37384aa7559ff14f531e6ab11e2182744
hmmei <ming197@qq.com>
2024-08-18 23:12:59 +0800

    binary search

---
+++ 33.search-in-rotated-sorted-array.cpp
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode id=33 lang=cpp
+ *
+ * [33] Search in Rotated Sorted Array
+ */
+
+// @lc code=start
+#include "bits/stdc++.h"
+using namespace std;
+class Solution {
+public:
+    int search(std::vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
+
+
+int main() {
+    Solution s;
+    vector<int> nums = {5,1,3};
+    int target = 3;
+    cout << s.search(nums, target) << endl;
+    return 0;
+}
+// @lc code=end
+
