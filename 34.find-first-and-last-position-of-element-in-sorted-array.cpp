4e7bdfd92d466bf81be805d55223cfc5ade59aa6
hmmei <ming197@qq.com>
2024-08-19 16:12:45 +0800

    binary search

---
+++ 34.find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=34 lang=cpp
+ *
+ * [34] Find First and Last Position of Element in Sorted Array
+ */
+
+// @lc code=start
+#include "bits/stdc++.h"
+using namespace std;
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        vector<int> res = {-1, -1};
+        if (nums.size() == 0) return res;
+        // find left bound
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) left = mid + 1;
+            else right = mid;
+        }
+        res[0] = nums[left] == target ? left : -1;
+        right = nums.size() - 1;
+        // find right bound
+        while (left < right)
+        {
+            int mid = left + (right - left + 1) / 2;
+            if (nums[mid] > target) right = mid - 1;
+            else left = mid;
+        }
+        res[1] = nums[right] == target ? right : -1;
+        return res;
+    }
+};
+// @lc code=end
+
