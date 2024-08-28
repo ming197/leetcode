cf1b988b7ab84504aaf056258aa413cb91b9ff60
hmmei <ming197@qq.com>
2024-08-29 00:32:20 +0800

    bitwise and manipulation && use the condition of no occurring of zero

---
+++ 201.bitwise-and-of-numbers-range.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=201 lang=cpp
+ *
+ * [201] Bitwise AND of Numbers Range
+ */
+
+// @lc code=start
+#include "bits/stdc++.h"
+using namespace std;
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            int mask = 1 << i;
+            // the maximum number of delta should be less than mask
+            // or there must be a carry in the i-th bit, it will be 0
+            if ((right - left) < mask && (left & mask) && (right & mask)) {
+                result |= mask;
+            }
+        }
+    }
+};
+// @lc code=end
+
