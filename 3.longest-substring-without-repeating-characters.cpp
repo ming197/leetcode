1f63579417aa87dd84ea797861086d085cca0c9c
hmmei <ming197@qq.com>
2024-11-02 19:59:44 +0800

    refine

--- 3.longest-substring-without-repeating-characters.cpp
+++ 3.longest-substring-without-repeating-characters.cpp
@@ -11,21 +11,26 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int n = s.size();
-        unordered_map<char, int> mp;
-        // substr: [l, r)
-        int l = 0, r = 0, res = 0;
-        while (l <= r && r < n)
-        {
-            if (mp.find(s[r]) == mp.end() || mp[s[r]] == 0)
-            {
-                ++mp[s[r++]];
-                res = max(res, r - l);
+        int left = 0, right = 0;
+        // char2index: the index of the character in the string
+        unordered_map<char, int> char2index;
+        int maxLen = 0;
+        while (++right < n + 1) {
+            char c = s[right - 1];
+            if (char2index.find(c) == char2index.end()) {
+                maxLen = max(maxLen, right - left);
             } else {
-                --mp[s[l]];
-                ++l;
+                // the character is already in the substring
+                // remove the character from the left to the index of the character
+                int index = char2index[c];
+                while (left <= index) {
+                    char2index.erase(s[left++]);
+                }
             }
+            // update the index of the character
+            char2index[c] = right - 1;
         }
-        return res;
+        return maxLen;
     }
 };
 // @lc code=end
