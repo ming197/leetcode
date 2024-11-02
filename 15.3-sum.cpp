80ec72ec13634207cc80c36c5ca1bf3abeec1c86
hmmei <ming197@qq.com>
2024-11-02 17:03:18 +0800

    sort and remove the duplicate triplet

--- 15.3-sum.cpp
+++ 15.3-sum.cpp
@@ -15,34 +15,29 @@ public:
         // sort first
         sort(nums.begin(), nums.end());
         // 1. fix the first number
-        for (int i = 0; i < nums.size(); ++i) {
+        for (int i = 0; i < nums.size() - 2; ++i) {
             // 2. skip the duplicate triplet
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            int l = i + 1, r = nums.size() - 1;
-            while (l < r)
-            {
-               if (nums[i] + nums[l] + nums[r] == 0) {
-                    res.push_back(vector<int>{nums[i], nums[l], nums[r]});
-                    ++l;
-                    // 2. skip the duplicate triplet
-                    while (l < r && nums[l] == nums[l - 1]) {
-                        ++l;
-                    }
-               } else if (nums[i] + nums[l] + nums[r] > 0) {
+            for (int l = i + 1; l < nums.size() - 1; ++l) {
+                // 3. skip the duplicate pair
+                if (l > i + 1 && nums[l] == nums[l - 1]) {
+                    continue;
+                }
+                int r = nums.size() - 1;
+                // 4. move the right pointer to find the targetable number
+                while (l < r && nums[i] + nums[l] + nums[r] > 0) {
                     --r;
-                    // 2. skip the duplicate triplet
-                    while (l < r && nums[r] == nums[r + 1]) {
-                        --r;
-                    }
-               } else {
-                    ++l;
-                    // 2. skip the duplicate triplet
-                    while (l < r && nums[l] == nums[l - 1]) {
-                        ++l;
-                    }
-               }
+                }
+                // 5. if the left pointer meets the right pointer, break
+                if (l == r) {
+                    break;
+                }
+                // 6. if the sum is 0, add the triplet to the result
+                if (nums[i] + nums[l] + nums[r] == 0) {
+                    res.push_back({nums[i], nums[l], nums[r]});
+                }
             }
         }
         return res;
