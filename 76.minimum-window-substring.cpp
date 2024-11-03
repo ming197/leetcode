1972e19d2ef7d28a00ef17098bd6f26d07411519
hmmei <ming197@qq.com>
2024-11-03 13:48:16 +0800

    add note

--- 76.minimum-window-substring.cpp
+++ 76.minimum-window-substring.cpp
@@ -22,16 +22,22 @@ public:
         while (++r < m)
         {
             temp_table[s[r]]++;
+            // only when the frequency of the character in the window is less than or equal to the frequency of the character in the target string
+            // the character is valid
+            // otherwise, the character is redundant
             if(hash_table.find(s[r]) != hash_table.end() && hash_table[s[r]] >= temp_table[s[r]]){
                 cntValid++;
             }
             while (cntValid == n && l <= r)
             {
+                // update the result when the valid count is equal to the target string length
                 if(r - l + 1 < res[1]){
                     res[0] = l;
                     res[1] = r - l + 1;
                 }
+                // move the left boudary of the window
                 temp_table[s[l]]--;
+                // update the valid count when the frequency of the character in the window is less than the frequency of the character in the target string
                 if(hash_table.find(s[l]) != hash_table.end() && temp_table[s[l]] < hash_table[s[l]]){
                    cntValid--;
                 }
