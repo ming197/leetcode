030540c872d27d11827d24950484ba1f61920e51
hmmei <ming197@qq.com>
2024-11-04 21:45:13 +0800

    refine

--- 48.rotate-image.cpp
+++ 48.rotate-image.cpp
@@ -13,22 +13,16 @@ public:
         int m = matrix.size(), n = matrix[0].size();
         // swap the matrix[i][j] and matrix[j][i]
         for (int i = 0; i < m; ++i) {
-            for (int j = 0; j <= i; ++j) {
-                swap(matrix, i, j, j, i);
+            for (int j = 0; j < i; ++j) {
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
         // swap the matrix[i][j] and matrix[i][n-j-1]
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n / 2; ++j) {
-                swap(matrix, i, j, i, n - j - 1);
+                swap(matrix[i][j], matrix[i][n - j - 1]);
             }
         }
-    }
-
-    void swap(vector<vector<int>> &matrix, int i, int j, int m, int n) {
-        int temp = matrix[i][j];
-        matrix[i][j] = matrix[m][n];
-        matrix[m][n] = temp;
     }
 };
 // @lc code=end
