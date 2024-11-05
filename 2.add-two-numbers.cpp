f960aedff161d0f07fd5fdc4bb88f706b2a111a3
hmmei <ming197@qq.com>
2024-11-05 23:50:00 +0800

    refine

--- 2.add-two-numbers.cpp
+++ 2.add-two-numbers.cpp
@@ -6,48 +6,45 @@
 
 // @lc code=start
 
-  struct ListNode {
-      int val;
-      ListNode *next;
-      ListNode() : val(0), next(nullptr) {}
-      ListNode(int x) : val(x), next(nullptr) {}
-      ListNode(int x, ListNode *next) : val(x), next(next) {}
-  };
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        // if any list is empty, return another linked list
-        if (!l1 || !l2) {
-            return l1 ? l1 : l2;
-        }
-        // create a dummy head node
-        ListNode* dummyHead = new ListNode();
-        ListNode* prev = dummyHead;
-        int x = 0;
-        // loop through both linked lists
-        while (l1 && l2) {
-            int val = (l1->val + l2->val + x) % 10;
-            x = (l1->val + l2->val + x) / 10;
-            prev->next = new ListNode(val);
-            prev = prev->next;
+        ListNode *dummy_head = new ListNode();
+        ListNode *prev = dummy_head;
+        int carry = 0;
+        while (l1 && l2)
+        {
+            ListNode *new_node = new ListNode((l1->val + l2->val + carry) % 10);
+            carry = (l1->val + l2->val + carry) / 10;
+            prev->next = new_node;
+            prev = new_node;
             l1 = l1->next;
             l2 = l2->next;
         }
-        // if any linked list is not empty, loop through it
-        ListNode* l3 = l1 ? l1 : l2;
-        // loop through the remaining linked list when one of the linked list is not empty or x > 0
-        // pay attention to the condition in the while loop
-        while (l3 || x > 0) {
-            int y = l3 ? l3->val : 0;
-            int val = (y + x) % 10;
-            x = (y + x) / 10;
-            prev->next = new ListNode(val);
-            prev = prev->next;
-            if (l3) l3 = l3->next;
+        l1 = l1 ? l1 : l2;
+        while (l1)
+        {
+            ListNode *new_node = new ListNode((l1->val + carry) % 10);
+            carry = (l1->val + carry) / 10;
+            prev->next = new_node;
+            prev = new_node;
+            l1 = l1->next;
         }
-
-        return dummyHead->next;
+        // if there is a carry left
+        if (carry)
+        {
+            ListNode *new_node = new ListNode(carry);
+            prev->next = new_node;
+        }
+        return dummy_head->next;
     }
 };
 
