b1be6cfd698b6d1463eadb32052aba1cd056e8d8
hmmei <ming197@qq.com>
2024-11-05 22:58:38 +0800

    refine

--- 21.merge-two-sorted-lists.cpp
+++ 21.merge-two-sorted-lists.cpp
@@ -15,29 +15,34 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+ struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        // dummy head
-        ListNode* dummyHead = new ListNode();
-        ListNode* previous_ptr = dummyHead;
-        while (l1 && l2) {
-            if (l1->val < l2->val) {
-                previous_ptr->next = l1;
-                previous_ptr = previous_ptr->next;
+        ListNode *dummy_head = new ListNode();
+        ListNode *prev_ptr = dummy_head;
+        while (l1 && l2)
+        {
+            if (l1->val < l2->val)
+            {
+                prev_ptr->next = l1;
                 l1 = l1->next;
-            } else {
-                previous_ptr->next = l2;
-                previous_ptr = previous_ptr->next;
+            }
+            else
+            {
+                prev_ptr->next = l2;
                 l2 = l2->next;
             }
+            prev_ptr = prev_ptr->next;
         }
-        if (l1) {
-            previous_ptr->next = l1;
-        } else {
-            previous_ptr->next = l2;
-        }
-        return dummyHead->next;
+        prev_ptr->next = l1 ? l1 : l2;
+        return dummy_head->next;
     }
 };
 // @lc code=end
