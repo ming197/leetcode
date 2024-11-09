201086acf947b2b252e63e23cf428f1c10fe2066
hmmei <ming197@qq.com>
2024-11-09 14:40:03 +0800

    pre_order && in_order

--- 105.construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105.construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -21,22 +21,19 @@ using namespace std;
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        return buildTree(preorder, 0, preorder.size() - 1, inorder, 0, preorder.size() - 1);
+        return buildTree(preorder, inorder, 0, preorder.size() - 1, 0, inorder.size() - 1);
     }
 
-    TreeNode* buildTree(vector<int>& preorder, int pre_start, int pre_end, vector<int>& inorder, int in_start, int in_end) {
-        if (pre_start > pre_end) {
-            return nullptr;
-        }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder, int pre_start, int pre_end, int in_start, int in_end) {
+        if (pre_start > pre_end) return nullptr;
         int root_val = preorder[pre_start];
         int root_index = in_start;
-        while (inorder[root_index] != root_val) {
-            root_index++;
-        }
+        // find the root index in inorder
+        while (inorder[root_index] != root_val) root_index++;
+        int left_size = root_index - in_start;
         TreeNode *root = new TreeNode(root_val);
-        int left_size = root_index - in_start ;
-        root->left = buildTree(preorder, pre_start + 1, pre_start + left_size, inorder, in_start, root_index - 1);
-        root->right = buildTree(preorder, pre_start + left_size + 1, pre_end, inorder, root_index + 1, in_end);
+        root->left = buildTree(preorder, inorder, pre_start + 1, pre_start + left_size, in_start, root_index - 1);
+        root->right = buildTree(preorder, inorder, pre_start + left_size + 1, pre_end, root_index + 1, in_end);
         return root;
     }
 };
