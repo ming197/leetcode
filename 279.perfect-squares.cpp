1cc168303d01153d1963da30f4985f8da7a00f79
hmmei <ming197@qq.com>
2024-11-15 00:08:37 +0800

    add note

--- 279.perfect-squares.cpp
+++ 279.perfect-squares.cpp
@@ -13,12 +13,14 @@ public:
         vector<int> dp(n + 1);
         // dp[i] = the least number of perfect square numbers which sum to i
         // dp[i] = min(dp[i - j * j] + 1), 1 <= j * j <= i
+        // dp[0] = 0, which is used to 
         dp[0] = 0;
         for (int i = 1; i <= n; ++i)
         {
             int min_num = INT_MAX;
             // dp_(i - delta), delta = j * j
             // find the min_num
+            // cannot use the binary search here, because the dp[i] is not sorted
             for (int j = 1; j * j <= i; ++j)
             {
                 min_num = min(min_num, dp[i - j * j]);
